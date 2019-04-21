ee9d21e6940c2e51158468720832405e2b8efb39
Fernando <fercalderon37@gmail.com>
2019-04-21 11:01:38 -0600

    Add articles logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+5.times do
+  User.create!(email: FFaker::Internet.safe_email,
+               password: FFaker::Internet.password,
+               first_name: FFaker::Name.first_name,
+               last_name: FFaker::Name.last_name,
+               birthday: Time.now,
+               city: FFaker::Address.city,
+               country: FFaker::Address.country)
+end
+
+15.times do
+  Article.create!(author: User.order('RANDOM()').first,
+                  title: FFaker::Book.title,
+                  text: FFaker::Book.description)
+end
