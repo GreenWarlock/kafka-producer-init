f263bc12a81ea061ba46ca3f3679b76daa984a70
Fernando <fercalderon37@gmail.com>
2019-04-20 20:40:36 -0600

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      redirect_to users_path, notice: 'Story was successfully updated.'
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :birthday, :city, :country)
+  end
 end
--- db/migrate/20190421020041_add_fields_to_user.rb
+++ db/migrate/20190421020041_add_fields_to_user.rb
@@ -2,7 +2,7 @@ class AddFieldsToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name,  :string
-    add_column :users, :birthday,   :string
+    add_column :users, :birthday,   :date
     add_column :users, :city,       :string
     add_column :users, :country,    :string
   end
