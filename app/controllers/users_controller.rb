30ff45eeede2c7cea19a17611767759d8faa2d5d
Fernando <fercalderon37@gmail.com>
2019-04-20 20:46:55 -0600

    Add destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %w[edit update destroy]
+
   def index
     @users = User.all
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    user = User.find(params[:id])
-
-    if user.update(user_params)
-      redirect_to users_path, notice: 'Story was successfully updated.'
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user.destroy
+
+    redirect_to users_path, notice: 'User was successfully destroyed.'
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:email, :last_name, :first_name, :birthday, :city, :country)
+    params.require(:user).permit(:email, :last_name, :first_name, :birthday,
+                                 :city, :country)
   end
 end
