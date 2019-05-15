2fce62b78ac860a732b53345c8033736a3eeae78
Fernando <fercalderon37@gmail.com>
2019-05-14 19:51:48 -0600

    Add validator for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :visualized_statistics, class_name: 'Visualization', foreign_key: 'visualized_user_id'
   has_many :visualizer_statistics, class_name: 'Visualization', foreign_key: 'visualizer_user_id'
   has_many :articles, foreign_key: :author_id
+
+  validates_uniqueness_of :email
 end
